edff6dcc748b19d9e047ae8719773460035b043f
Cauã R. Pereira <7aauac@gmail.com>
2024-08-25 01:14:10 -0300

    20240825

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('dashboard');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('comments', CommentController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
